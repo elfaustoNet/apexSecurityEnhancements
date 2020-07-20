4455cd279110ef64afa5daf210e2a863efef9c25
Mike Faust <mike.faust@mfaust-ltm.internal.salesforce.com>
2020-07-20 09:57:20 -0400

    output format update

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 
-sfdx force:org:create -a apexSecurity -f ../config/project-scratch-def.json -d 2 -w 10 -s
+sfdx force:org:create -a apexSecurity -f ./config/project-scratch-def.json -d 2 -w 10 -s
 sfdx force:source:push -u apexSecurity
 sfdx force:user:permset:assign -u apexSecurity -n Apex_Security_Enhancement
 sfdx force:data:tree:import -p ./data/Plan.json
